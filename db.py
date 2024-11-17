86f5a5be7aab31157a24a3e68ff1f2286f5cb5e2
sabario <lucas378363462@gmail.com>
2024-11-17 17:38:59 +0200

    update error handling

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError  # Import SQLAlchemy exceptions
 from dotenv import load_dotenv
 import os
 
@@ -7,12 +8,17 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
 Session = sessionmaker(bind=engine)
 
 def get_db_connection():
     db_session = Session()
     try:
         yield db_session
+    except SQLAlchemyError as e:
+        print(f"An error occurred while handling the database operation: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
     finally:
         db_session.close()
